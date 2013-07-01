5059a7f747a8951e4bf231530b6c322872e126d1
Ma Tianxiao <matx2215@outlook.com>
2013-07-02 01:15:47 +0800

    answer recursion part-1

--- app/recursion.js
+++ app/recursion.js
@@ -3,7 +3,28 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     listFiles: function(data, dirName) {
+      var result = [];
+      var foundSub = false;
 
+      function list (data) {
+        if (dirName && dirName === data.dir) {
+          foundSub = true;
+        }
+
+        for (var i = 0; i < data.files.length; i++) {
+          if (data.files[i].dir) {
+            list(data.files[i]);
+          } else if (foundSub || !dirName) {
+            result.push(data.files[i]);
+          }
+        }
+
+        foundSub = false;
+      }
+
+      list(data, result);
+      
+      return result;
     },
 
     permute: function(arr) {
