4aca533b5ebe9ddca3e21e36daed2de3c535ae13
Ma Tianxiao <matx2215@outlook.com>
2013-07-01 23:20:34 +0800

    answer async

--- app/async.js
+++ app/async.js
@@ -3,11 +3,24 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
+      var deferred = $.Deferred();
 
+      deferred.resolve(value);
+
+      return deferred.promise();
     },
 
     manipulateRemoteData : function(url) {
+      var deferred = $.Deferred();
+      
+      $.get(url, function(data) {
+        var name = data.people.map(function(value) {
+          return value.name;
+        });
+        deferred.resolve(name.sort()); 
+      });
 
+      return deferred.promise();
     }
   };
 });
