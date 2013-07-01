ace7b6ea23e3e60db9674707d8f81b763e8778df
Ma Tianxiao <matx2215@outlook.com>
2013-07-02 00:03:13 +0800

    answer count

--- app/count.js
+++ app/count.js
@@ -3,6 +3,25 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      var timer;
+
+      function doCount () {
+        console.log(start++);
+
+        if (start <= end) {
+         timer = setTimeout(doCount, 100);
+        }
+      }
+
+      doCount();
+
+      return {
+        cancel: function () {
+          if (timer) {
+            clearTimeout(timer);
+          }
+        }
+      };
     }
   };
 });
