89da9ab117a0d0f8e66510a2c8c516b90452056a
Ma Tianxiao <matx2215@outlook.com>
2013-07-02 23:43:33 +0800

    answer permutation

--- app/recursion.js
+++ app/recursion.js
@@ -28,7 +28,27 @@ define(function() {
     },
 
     permute: function(arr) {
+      var result = [];
+      var temp = [];
 
+      function p (arr) {
+        for (var i = 0; i < arr.length; i++) {
+          var element = arr.splice(i, 1)[0];
+
+          temp.push(element);
+          if (arr.length === 0) {
+            result.push(temp.slice());
+          }
+
+          p(arr);
+          arr.splice(i, 0, element);
+          temp.pop();
+        }
+
+        return result;
+      }
+
+      return p(arr);
     }
   };
 });
