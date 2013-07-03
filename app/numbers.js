51ade5e600f28e54277f1748197ec8b0af34d4fa
BetaRabbit <matx2215@outlook.com>
2013-07-03 18:06:04 +0800

    answer number

--- app/numbers.js
+++ app/numbers.js
@@ -3,15 +3,20 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+      return 1 & (num >> (bit - 1));
     },
 
     base10: function(str) {
-
+      return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
+      var str = num.toString(2);
+      for (var i = 0, l = 8 - str.length; i < l; i++) {
+        str = '0' + str;
+      }
 
+      return str;
     },
 
     multiply: function(a, b) {
